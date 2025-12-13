9468741a816d41d2e7297f1d1a437c8e953a52d9
Aleksey Demchenkov <contact@pelfox.dev>
2025-12-13 17:18:14 +0300

    feat(config): implemented environment parsing, as well as fallbacks

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/Pelfox/go-shortener/internal/config"
+	"github.com/Pelfox/go-shortener/internal"
 	"github.com/Pelfox/go-shortener/internal/service"
 )
 
 func main() {
-	appConfig := config.ParseAppConfig()
+	appConfig := internal.ParseAppConfig()
 
 	server := service.NewServer(appConfig.Host, appConfig.URLPrefix)
 	if err := server.ServeHTTP(); err != nil {
